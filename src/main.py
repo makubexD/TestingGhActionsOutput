fe36ae7aa039585278cddbae075f34c85e3097a0
Kiefer Fernandez <129541980+KieferFernandez@users.noreply.github.com>
2025-02-11 15:54:53 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import time
+import os
 
 # for i in tqdm(range(100), ncols=100, disable=True):
 #     time.sleep(0.1)
@@ -14,7 +15,9 @@ import time
 #         progress.update(task, advance=1)
 #         time.sleep(0.1)
 
-
+print("BEGIN = To check if GH")
+print(os.getenv('GITHUB_ACTIONS'))
+print("END = To check if GH")
 
 for i in tqdm(range(900), miniters=10):
     time.sleep(0.1)
