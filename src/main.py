b708d8d24eafeadf6b0fce7b06de3cfca90b981e
Kiefer Fernandez Valladolid <Kiefer.Fernandez@tranzact.net>
2025-02-11 10:33:10 -0500

    Increase time for rich.progress

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ from rich.progress import Progress
 import time
 
 with Progress() as progress:
-    task = progress.add_task("[cyan]Processing...", total=100)
-    for i in range(100):
+    task = progress.add_task("[cyan]Processing...", total=900)
+    for i in range(900):
         progress.update(task, advance=1)
         time.sleep(0.1)
 
