e019f562be1f2f242ada183c51a75de1037db077
Kiefer Fernandez Valladolid <Kiefer.Fernandez@tranzact.net>
2025-02-11 10:35:12 -0500

    Increase/update to time for tqdm

--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
-# from tqdm import tqdm
-# import time
+from tqdm import tqdm
+import time
 
 # for i in tqdm(range(100), ncols=100, disable=True):
 #     time.sleep(0.1)
 
 
-from rich.progress import Progress
-import time
+# from rich.progress import Progress
+# import time
 
-with Progress() as progress:
-    task = progress.add_task("[cyan]Processing...", total=900)
-    for i in range(900):
-        progress.update(task, advance=1)
-        time.sleep(0.1)
+# with Progress() as progress:
+#     task = progress.add_task("[cyan]Processing...", total=900)
+#     for i in range(900):
+#         progress.update(task, advance=1)
+#         time.sleep(0.1)
 
 
 
-# for i in tqdm(range(100), miniters=10):
-#     time.sleep(0.1)
+for i in tqdm(range(900), miniters=10):
+    time.sleep(0.1)
 
 
 
